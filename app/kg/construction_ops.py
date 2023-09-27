b559de5c11d92f72364dd417c02aa5ac6667cf75
Ipek Baris Schlicht <ipek.baris-schlicht@dw.com>
2023-09-27 12:31:46 +0200

    add wiki descrtiption scraper

--- app/kg/construction_ops.py
+++ app/kg/construction_ops.py
@@ -8,11 +8,15 @@ from rdflib import Graph, URIRef, Namespace
 from rdflib.term import Literal
 from rdflib.namespace import OWL, RDF, RDFS
 from pathlib import Path
+from bs4 import BeautifulSoup
+from app.kg.utils import _load_graph, WD, WIKIBASE, WDT, WIKIDATA, SCHEMA
+from app.kg.search_ops import fetch_all_osm_tags
 
 cache = Cache('tmp')
 WIKIDATA_DOMAIN = 'http://www.wikidata.org/entity/'
 OSM_SEARCH_ENDPOINT = 'https://wiki.openstreetmap.org/w/api.php?action=wbsearchentities&format=json&search=TAG&language=en'
 OSM_DATA_ENDPOINT = 'https://wiki.openstreetmap.org/wiki/Special:EntityData/TAG_ID.ttl'
+OSM_TAG_WIKI_PAGE_ENDPOINT = 'https://wiki.openstreetmap.org/wiki/Tag:OSM_TAG'
 OSM_PREFIX_REGEX = r'^(Tag:|Key:)'
 
 
@@ -97,23 +101,19 @@ def merge_kg(input_file, output_file):
 def create_kg(wikidata_ref, mapping_ref, osm_ref, output_file):
     wiki_g = Graph()
 
-    WD = Namespace("https://wiki.openstreetmap.org/entity/")
-    WIKIBASE =  Namespace("http://wikiba.se/ontology#")
-    WIKIDATA = Namespace("http://www.wikidata.org/entity/")
-
     wiki_g.bind("wd", WD)
     wiki_g.bind("wikibase", WIKIBASE)
     wiki_g.bind("wikidata", WIKIDATA)
 
     mapping_data = pd.read_csv(mapping_ref, sep='\t')
-    wikidata =  pd.read_csv(wikidata_ref, sep='\t')
+    wikidata = pd.read_csv(wikidata_ref, sep='\t')
 
     for _, row in wikidata.iterrows():
         wikidata_id = WIKIDATA[row["wikiData"].split('/')[-1]]
 
         tag_info = row['tagInfo']
         tag_info = re.sub(OSM_PREFIX_REGEX, '', tag_info)
-        osm_id = mapping_data[mapping_data["tag_info"]==tag_info]["osm_id"]
+        osm_id = mapping_data[mapping_data["tag_info"] == tag_info]["osm_id"]
 
         if len(osm_id) == 0:
             continue
@@ -122,7 +122,7 @@ def create_kg(wikidata_ref, mapping_ref, osm_ref, output_file):
 
         wiki_g.add((wikidata_id, RDF.type, WIKIBASE.Item))
         wiki_g.add((wikidata_id, RDFS.label, Literal(row["wikiDataLabel"])))
-        wiki_g.add((WD[osm_id],OWL.sameAs, wikidata_id ))
+        wiki_g.add((WD[osm_id], OWL.sameAs, wikidata_id))
 
     osm_g = Graph()
     osm_g.parse(osm_ref)
@@ -130,8 +130,70 @@ def create_kg(wikidata_ref, mapping_ref, osm_ref, output_file):
 
     merged_g.serialize(destination=output_file)
 
-def get_additional_values(key):
-    pass
+
+@cache.memoize()
+def scrape_osm_wiki_description(tag_info):
+    osm_endpoint = OSM_TAG_WIKI_PAGE_ENDPOINT.replace('OSM_TAG', tag_info)
+    response = requests.get(osm_endpoint)
+
+    if response.status_code != 200:
+        return None
+
+    return response.content
+
+
+def process_osm_wiki_description(wiki_description):
+    if not wiki_description:
+        return None
+
+    # Parse the HTML content
+    soup = BeautifulSoup(wiki_description, 'html.parser')
+
+    descriptions = []
+
+    content_div = soup.find('div', {'id': 'mw-content-text', 'class': 'mw-body-content mw-content-ltr'})
+    td_elem = content_div.find('tr', class_='d_description content')
+
+    if td_elem:
+        description_text = td_elem.get_text(strip=True)
+
+        if description_text:
+            descriptions.append(description_text)
+
+    content_paraghs = content_div.find_all('p')
+    for content_paragh in content_paraghs:
+        content_paragh = content_paragh.get_text(strip=True)
+        if len(content_paragh) > 0:
+            descriptions.append(content_paragh)
+
+    return None if len(descriptions) == 0 else descriptions
+
+
+def get_additional_values(input_file, output_file):
+    '''
+    This function reads the osm tags and extend their
+    :param input_file:
+    :param output_file:
+    :return:
+    '''
+    g = _load_graph(input_file)
+
+    for s, _, n in g.triples((None, WDT["P19"], None)):
+        print(f"Requesting {n}")
+        wiki_description = scrape_osm_wiki_description(tag_info=n)
+
+        if not wiki_description:
+            print(f'{s} does not have a Wiki description')
+
+        wiki_descriptions = process_osm_wiki_description(wiki_description)
+
+        if wiki_descriptions:
+            for wiki_description in wiki_descriptions:
+                g.add((s, SCHEMA.description, Literal(wiki_description, lang="en")))
+        else:
+            print(f'{s} does not have a Wiki description')
+
+    g.serialize(destination=output_file)
 
 
 if __name__ == '__main__':
@@ -148,6 +210,9 @@ if __name__ == '__main__':
     parser.add_argument('--merge_kg', action='store_true')
     parser.add_argument('--create_kg', action='store_true')
 
+    # enriched descriptions
+    parser.add_argument('--additional_values', choices=["osm_wiki_descriptions"])
+
     args = parser.parse_args()
 
     if args.map:
@@ -162,4 +227,8 @@ if __name__ == '__main__':
         merge_kg(input_file=args.input_file, output_file=args.output_file)
 
     if args.create_kg:
-        create_kg(wikidata_ref=args.wikidata_ref, mapping_ref=args.mapping_ref, osm_ref=args.osm_ref, output_file=args.output_file)
+        create_kg(wikidata_ref=args.wikidata_ref, mapping_ref=args.mapping_ref, osm_ref=args.osm_ref,
+                  output_file=args.output_file)
+
+    if args.additional_values:
+        get_additional_values(input_file=args.input_file, output_file=args.output_file)
--- scripts/osm_construct_ops.sh
+++ scripts/osm_construct_ops.sh
@@ -13,18 +13,25 @@
 #--fetch
 
 
-echo merge knowledge graphs for constructing
+#echo merge knowledge graphs for constructing
+#
+#python -m app.kg.construction_ops \
+#--input_file datasets/osm/raw \
+#--output_file datasets/osm/osm_kg.ttl \
+#--merge_kg
+#
+#
+#echo create a custom OSM kg combining selected wikidata combination and osm combination
+#python -m app.kg.construction_ops \
+#--wikidata_ref datasets/wikidata_v2.tsv \
+#--osm_ref datasets/osm/osm_kg.ttl \
+#--mapping_ref datasets/osm_wikidata_mapping_v3.tsv \
+#--output_file datasets/osm/osm_enriched_kg.ttl \
+#--create_kg
 
+
+echo enrich the kgs with descriptions from their wiki page
 python -m app.kg.construction_ops \
---input_file datasets/osm/raw \
---output_file datasets/osm/osm_kg.ttl \
---merge_kg
-
-
-echo create a custom OSM kg combining selected wikidata combination and osm combination
-python -m app.kg.construction_ops \
---wikidata_ref datasets/wikidata_v2.tsv \
---osm_ref datasets/osm/osm_kg.ttl \
---mapping_ref datasets/osm_wikidata_mapping_v3.tsv \
---output_file datasets/osm/osm_enriched_kg.ttl \
---create_kg
+--input_file datasets/osm/osm_enriched_kg_v2.ttl \
+--output_file datasets/osm/osm_enriched_kg_v3.ttl \
+--additional_values osm_wiki_descriptions
